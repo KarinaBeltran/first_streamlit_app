a05b5262c2fa057efa8e8a0e9be5180da3cb1da5
Karina BC <110939702+KarinaBeltran@users.noreply.github.com>
2022-08-09 17:08:41 -0500

    Update streamlit_app.py
    Add icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
-streamlit.title('Breakfast Menu')
-streamlit.title('Omega 3 & Blueberry Omelet')
-streamlit.title('Kale, Spinach & Rocket Smoothie ')
-streamlit.title('Hard-Boiled Free-Range Egg')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Omelet')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie ')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
